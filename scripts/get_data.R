a7565e1a39c4b252441853ccea1e4533c6be5d13
Mark Andrews <mjandrews.org@gmail.com>
2024-09-05 08:37:29 +0200

    Update script

--- scripts/get_data.R
+++ scripts/get_data.R
@@ -23,3 +23,24 @@ eyefix_df_avg_targ <- dplyr::filter(eyefix_df_avg, Object == 'Target')
 DOC_df <- readr::read_csv(GH("data/DoctorVisits.csv"))
 
 gssvocab_df <- readr::read_csv(GH("data/gssvocab.csv"))
+
+set.seed(1001)
+eyefix_df_avg_targ2 <- 
+  mutate(eyefix_df_avg_targ, 
+         mean_fix = mean_fix + rnorm(n=n(), sd = 0.1))
+
+eyefix_df_avg2 <- 
+  mutate(eyefix_df_avg, 
+         mean_fix = mean_fix + rnorm(n=n(), sd = 0.1),
+         Object = factor(Object))
+
+set.seed(101)
+mono_df <- tibble(x = seq(-2, 2, length.out = 20),
+                  y = 0.5 + 1.5 * x + rnorm(length(x))
+)
+
+golf_df <- readr::read_csv(GH("data/golf_putts.csv"))
+eyetrackr_df <- readr::read_csv(GH("data/eyetrackr.csv")) %>% 
+  mutate(across(c(subject, condition, target), as.factor))
+
+meuse_df <- readr::read_csv(GH('data/meuse.csv'))
--- scripts/utils.R
+++ scripts/utils.R
@@ -3,3 +3,27 @@ AIC_C <- function(model){
   N <- nrow(model$model)
   AIC(model) + (2*K*(K+1))/(N-K-1)
 }
+
+loo_lm_poly <- function(degree, data_df){
+  
+  f <- 'mean_fix ~ poly(Time, degree = %d)'
+  n <- nrow(data_df)
+  
+  loocv <- function(i){
+    
+    train_df <- slice(data_df, -i)
+    test_df <- slice(data_df, i)
+    
+    M <- lm(formula(sprintf(f, degree)), data = train_df)
+    predictions <- add_predictions(data = test_df, model = M, var = 'y_hat')
+    
+    mutate(predictions,
+           lpd = dnorm(mean_fix, mean = y_hat, sd = sigma(M), log = T)
+    ) %>% pull(lpd) %>% unlist()
+    
+  }
+  
+  # elp
+  pointwise_elpd <- map_dbl(seq(n), loocv)
+  c(elpd = -2*sum(pointwise_elpd), se = -2 * sqrt(n * var(pointwise_elpd)))
+}
